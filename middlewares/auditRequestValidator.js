e1233a67744a9c18a95776ddb8dd460ab056a744
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2022-11-17 23:46:55 +0200

    adding audit request validator

---
+++ middlewares/auditRequestValidator.js
@@ -0,0 +1,25 @@
+const { AUDIT_ACTIONS } = require("../models/associations")
+const { ROLES } = require("../models/role")
+
+const auditCheck = async (req, res, next) => {
+    const allowedEntities = ['user', 'kindergarten', 'register_application', 'child']
+    const allowedOrderOptions = ['desc', 'asc']
+
+    if (!allowedEntities.includes(req.params.entity)) {
+        return res.status(400).send({ mgs: `${req.params.entity} Entity is not supported!` })
+    }
+
+    if (req.query.action != undefined && !Object.values(AUDIT_ACTIONS).includes(req.query.action)) {
+        return res.status(400).send({ mgs: `${req.query.action} Action is not supported!` })
+    }
+
+    if (req.query.order != undefined && !allowedOrderOptions.includes(req.query.order)) {
+        return res.status(400).send({ mgs: `${req.query.order} Order option is not supported!` })
+    }
+
+    // check if belongs
+
+    next()
+}
+
+module.exports = auditCheck
