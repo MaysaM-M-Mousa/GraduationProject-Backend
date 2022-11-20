c7a5f5d4a8ade8c7890fe1742b239760ccda0a04
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2022-11-20 22:15:13 +0200

    subscription-system: adding service controller/logic

---
+++ controllers/service.js
@@ -0,0 +1,85 @@
+const { Service } = require('../models/associations')
+
+exports.createService = async (req, res) => {
+    try {
+        const service = new Service(req.body)
+
+        await service.save()
+
+        res.status(201).send(service)
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
+exports.getServiceById = async (req, res) => {
+    try {
+        const service = await Service.findOne({ where: { id: req.params.id } })
+
+        if (!service) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(service)
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
+exports.getAllServices = async (req, res) => {
+    const MAX_PAGE_SIZE = 10
+
+    const pageNumber = Number((req.query.pageNumber == undefined) ? 1 : req.query.pageNumber)
+    const pageSize = Number((req.query.pageSize <= MAX_PAGE_SIZE) ? req.query.pageSize : MAX_PAGE_SIZE)
+
+    try {
+        const services = await Service.findAndCountAll({
+            offset: (pageNumber - 1) * pageSize,
+            limit: pageSize,
+            distinct: true
+        })
+
+        res.status(200).send(services)
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
+exports.updateService = async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'description']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ 'error': 'Invalid updates!' })
+    }
+
+    try {
+        const service = await Service.findOne({ where: { id: req.params.id } })
+
+        if (service == null) {
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => service[update] = req.body[update])
+        await service.save()
+
+        res.send(service)
+
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
+exports.deleteService = async (req, res) => {
+    try {
+        const result = await Service.destroy({ where: { id: req.params.id } })
+
+        if (!result) {
+            return res.status(404).send()
+        }
+        res.status(200).send()
+    } catch (e) {
+        res.status(500).send()
+    }
+}
